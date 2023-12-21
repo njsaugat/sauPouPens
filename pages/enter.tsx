a0dbfa4eda6d8ed1d8b9b3401a511ee581ec17cd
njsaugat <njsaugat@gmail.com>
2023-12-21 17:05:44 +0545

    enter page

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,24 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import Link from "next/link";
 const EnterPage = () => {
-  return <main>Signup</main>;
+  const inputRef = useRef<HTMLInputElement>(null);
+  return (
+    <main className="flex flex-col">
+      <input type="text" ref={inputRef} />
+      <Link
+        href={{
+          pathname: "/[username]",
+          query: { username: inputRef?.current?.value ?? "John Doe" },
+        }}
+      >
+        <button className="cursor-pointer self-end w-fit border-2 ">
+          Find Person
+        </button>
+      </Link>
+      <br />
+      Signup
+    </main>
+  );
 };
 
 export default EnterPage;
