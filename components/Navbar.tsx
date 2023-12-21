b082398b2f55e65e83aebfa40820d118b376c3a5
njsaugat <njsaugat@gmail.com>
2023-12-21 17:43:01 +0545

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,39 @@
-import React from "react";
-
+import Link from "next/dist/client/link";
+// import Image from 'next/image'
 const Navbar = () => {
+  const [user, username] = [null, null];
   return (
     <nav className="navbar">
       <ul>
-        <button className="btn-logo">SauPou</button>
-        <li>Log in</li>
+        <li>
+          <Link href={"/"}>
+            <button className="btn-logo">SauPou</button>
+          </Link>
+        </li>
+        <div>
+          {username ? (
+            <div className="flex items-center">
+              <li className="push-left">
+                <Link href="/admin">
+                  <button className="btn-blue">Write Posts</button>
+                </Link>
+              </li>
+              <li>
+                <Link href={`/${username}`}>
+                  <img src="https://shorturl.at/gmvDL" />
+                </Link>
+              </li>
+            </div>
+          ) : (
+            <>
+              <li>
+                <Link href={"/enter"}>
+                  <button className="btn-blue">Log in</button>
+                </Link>
+              </li>
+            </>
+          )}
+        </div>
       </ul>
     </nav>
   );
