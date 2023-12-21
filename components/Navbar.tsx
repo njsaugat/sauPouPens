f58215561ab9039db72d7b27cd033876361e9661
njsaugat <njsaugat@gmail.com>
2023-12-21 17:05:30 +0545

    push navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <button className="btn-logo">SauPou</button>
+        <li>Log in</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
